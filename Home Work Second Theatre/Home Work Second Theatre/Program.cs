c70a4dbb94f9292700c9ad3202de00beec876435
Dariana <dariana.rr@mail.ru>
2019-11-16 00:24:22 +1000

    Add some new functions

--- Home Work Second Theatre/Home Work Second Theatre/Program.cs
+++ Home Work Second Theatre/Home Work Second Theatre/Program.cs
@@ -9,6 +9,8 @@ namespace Home_Work_Second_Theatre
         {
             StangingWork.AddConcert("Gore ot uma", "Griboedov", "staging");
             StangingWork.SortType("staging");
+            TicketWork.AddTicets("Gore ot uma", "big scene", "2019.12.12 18:30", 750);
+            TicketWork.ShowAll();
         }
     }
 }
--- Home Work Second Theatre/Work Theatre/TicketWork.cs
+++ Home Work Second Theatre/Work Theatre/TicketWork.cs
@@ -7,7 +7,7 @@ namespace Work_Theatre
     {
         public static List<Ticket> tickets = new List<Ticket>();
         
-        public static void AddTicets(string name, string hall, DateTime time, decimal price) // Добавляет концерт
+        public static void AddTicets(string name, string hall, string time, decimal price) // Добавляет концерт
         {
             if (hall == "small scene" || hall == "big scene")
             {
@@ -15,6 +15,28 @@ namespace Work_Theatre
                 return;
             }
             Console.WriteLine("Sorry, you wrote the wrong hall");
+        }
+        
+        public static int FindId(string name)
+        {
+            return(tickets.FindIndex(tickets => tickets.Name == name));
+        }
+
+        public static void Delete(string name)
+        {
+            tickets.RemoveAt(FindId(name));
+        }
+
+        public static void ShowAll()
+        {
+            for (int i = 0; i < stagings.Count; i++)
+            {
+                Console.WriteLine( $"Name {stagings[FindId(tickets[i].Name)].Type} is: {tickets[i].Name} \r\n" +
+                                   $"Director {stagings[FindId(tickets[i].Name)].Type} is: {stagings[FindId(tickets[i].Name)].Director} \r\n" +
+                                   $"Hall is: {tickets[i].Hall} \r\n" +
+                                   $"Price is: {tickets[i].Price} \r\n" +
+                                   $"Date {stagings[FindId(tickets[i].Name)].Type} is: {tickets[i].Time}");
+            }
         }
     }
 }
