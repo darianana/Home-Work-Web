9c80c7f20201ed3978e384965e034af6a43b758b
Dariana <dariana.rr@mail.ru>
2019-12-14 22:07:30 +1000

    Fifth task

--- Third Home Work/fifth/fifth/RoutingMiddleware.cs
+++ Third Home Work/fifth/fifth/RoutingMiddleware.cs
@@ -1,7 +1,41 @@
-﻿namespace fifth
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace fifth
 {
     public class RoutingMiddleware
     {
-        
+        private readonly RequestDelegate _next;
+
+        public RoutingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            string path = context.Request.Path.Value.ToLower();
+            if (path == "/task1")
+            {
+                await context.Response.WriteAsync("Hello World!");
+            }
+            else if (path == "/task2")
+            {
+                double x = 10;
+                double k = 100;
+                double n = 0;
+                n = Math.Log10(Math.Sin(Math.PI / x)) * k;
+                await context.Response.WriteAsync($"x = 10\r\n" +
+                                                  $"k = 100\r\n" +
+                                                  $"lg(sin(Pi/x) * k = {n}");
+            }
+            else
+            {
+                context.Response.StatusCode = 404;
+            }
+
+            //await _next.Invoke(context);
+        }
     }
 }
--- Third Home Work/fifth/fifth/Startup.cs
+++ Third Home Work/fifth/fifth/Startup.cs
@@ -16,13 +16,19 @@ namespace fifth
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseMiddleware<ErrorHandlingMiddleware>();
             app.UseMiddleware<AuthenticationMiddleware>();
             app.UseMiddleware<RoutingMiddleware>();
+            app.UseRouting();
+
+            app.UseAuthorization();
+            
         }
     }
 }
