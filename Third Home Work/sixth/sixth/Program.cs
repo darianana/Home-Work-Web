c70d6e16321fe8a5ac848e1c2ffd168e2ef7066a
Dariana <dariana.rr@mail.ru>
2019-12-15 23:35:10 +1000

    Sixth task

--- Third Home Work/sixth/sixth/Program.cs
+++ Third Home Work/sixth/sixth/Program.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 
 namespace sixth
 {
--- Third Home Work/sixth/sixth/Startup.cs
+++ Third Home Work/sixth/sixth/Startup.cs
@@ -21,17 +21,9 @@ namespace sixth
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseStaticFiles(); // добавляем поддержку статических файлов
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
-            });
+            app.Run(async (context) => { await context.Response.WriteAsync("Hello World"); });
         }
     }
 }
