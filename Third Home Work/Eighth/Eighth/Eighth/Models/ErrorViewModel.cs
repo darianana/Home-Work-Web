860828b54d220b83bc0c9a406432ca4e3aec7e11
Dariana <dariana.rr@mail.ru>
2019-12-20 20:40:17 +1000

    Eigth task

---
+++ Third Home Work/Eighth/Eighth/Eighth/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Eighth.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Third Home Work/Eighth/Eighth/Eighth/Models/MobileContext.cs
@@ -0,0 +1,17 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+ 
+namespace Eighth.Models
+{
+    public class MobileContext : DbContext
+    {
+        public DbSet<Phone> Phones { get; set; }
+        public DbSet<Order> Orders { get; set; }
+ 
+        public MobileContext(DbContextOptions<MobileContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
